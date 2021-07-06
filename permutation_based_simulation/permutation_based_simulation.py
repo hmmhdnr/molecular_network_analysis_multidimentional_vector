48c6375581b483b5d0687b6d04f887753a7c422e
hmmhdnr <86830867+hmmhdnr@users.noreply.github.com>
2021-07-06 13:28:39 +0900

    Add files via upload

--- permutation_based_simulation/permutation_based_simulation.py
+++ permutation_based_simulation/permutation_based_simulation.py
@@ -1,74 +1,87 @@
 import pandas as pd
-from itertools import combinations, groupby
+from itertools import groupby
 from multiprocessing import Pool
 import time
 import datetime
 import os
 import random
 
-bootstrap_N = 1000000
-Dir_output = 'result'
+# permutation-based simulation
+# 2021/5/27 by H.Homma
+
+ListSize = 1965
+Size_A = 73
+Size_B = 62
+Expected_Overwrap_Size = 43
+bootstrap_N = 10000
+Dir_output = 'result_from_all_proteins'
 cpu_count = min(16, os.cpu_count())
 TaskID = 0
 
 import argparse
 parser = argparse.ArgumentParser()
-parser.add_argument("-B", help="number of bootstrap samples", type=int)
 parser.add_argument("--task_id", help="Task ID for parallel", type=int)
+parser.add_argument("--size_a", help="size of list_A", type=int)
+parser.add_argument("--size_b", help="size of list_B", type=int)
+parser.add_argument("--population_size", help="size of population", type=int)
+parser.add_argument("-B", help="number of bootstrap trials", type=int)
+parser.add_argument("-E", help="Expected overwrap size", type=int)
+parser.add_argument("-d", help="output directory", type=str)
 args = parser.parse_args()
 
-if args.B is not None:
-    bootstrap_N = args.B
 if args.task_id is not None:
     TaskID = args.task_id
+if args.size_a is not None:
+    Size_A = args.size_a
+if args.size_b is not None:
+    Size_B = args.size_b
+if args.population_size is not None:
+    ListSize = args.population_size
+if args.B is not None:
+    bootstrap_N = args.B
+if args.E is not None:
+    Expected_Overwrap_Size = args.E
+if args.d is not None:
+    Dir_output = args.d
+if not os.path.isdir(Dir_output):
+    os.makedirs(Dir_output)
+
+def init(_a, _b, _l):
+    global A, B, ALL
+    A = _a
+    B = _b
+    ALL = _l
 
 def split_iter(iterable, N=3):
     for i, item in groupby(enumerate(iterable), lambda x: x[0] // N):
         yield (x[1] for x in item)
 
-def init(_comb):
-    global COMBINED, LIST_LEN
-    COMBINED = _comb
-    LIST_LEN = len(_comb)
-
-def overwrap_ratio(lst_idx):
-    lst = [COMBINED[x] for x in lst_idx]
-    rest = [COMBINED[x] for x in range(LIST_LEN) if x not in lst_idx]
-    intersect = set(rest) & set(lst)
-    return len(lst), len(rest), len(intersect), len(intersect)/len(COMBINED), ','.join(intersect)
+def overwrap_ratio(idx):
+    la = random.sample(range(ALL), A)
+    lb = random.sample(range(ALL), B)
+    intersect = sorted(list(set(la) & set(lb)))
+    union = sorted(list(set(la) | set(lb)))
+    return idx, len(la), len(lb), len(intersect), len(union), len(intersect)/len(union), ','.join([str(x) for x in intersect]), ','.join([str(x) for x in union])
 
 def overwrap_ratio_wrapper(args):
     return overwrap_ratio(*args)
 
 if __name__ == '__main__':
-    path_ad = "ad_core_nodes.csv"
-    path_ftld = "ftld_core_nodes.csv"
-    path_common = "common_core_nodes.csv"
+    expected_list_size = Size_A + Size_B - Expected_Overwrap_Size
+    expected_ratio = Expected_Overwrap_Size / expected_list_size
+    print("bootstrap size_A = %d, size_B = %d from population of size %d" % (Size_A, Size_B, ListSize))
+    print("expected list size = %d, expected overwrap size = %d, expected overwrap ratio = %f" % (expected_list_size, Expected_Overwrap_Size, expected_ratio))
 
-    ad = pd.read_table(path_ad, sep=',', usecols=['FullName', 'GeneSymbol', 'shared name'])
-    ftld = pd.read_table(path_ftld, sep=',', usecols=['FullName', 'GeneSymbol', 'shared name'])
-    common = pd.read_table(path_common, sep=',', usecols=['FullName', 'GeneSymbol', 'shared name'])
+    pool = Pool(cpu_count, initializer=init, initargs=(Size_A, Size_B, ListSize))
+    result = pool.map(overwrap_ratio, range(bootstrap_N))
 
-    ad_proteins = list(ad.loc[:,'shared name'])
-    ftld_proteins = list(ftld.loc[:,'shared name'])
-    common_proteins = list(common.loc[:,'shared name'])
-    print("#ad = %d, #ftld = %d, #common = %d" % (len(ad_proteins), len(ftld_proteins), len(common_proteins)))
-
-    combined_list = ad_proteins + ftld_proteins
-    x = ','.join(combined_list)
-    print("#combined list = %d" % len(combined_list))
-
-    pool = Pool(cpu_count, initializer=init, initargs=(combined_list,))
-    result = pool.map(overwrap_ratio, [random.sample(range(len(combined_list)), k=len(ftld_proteins)) for x in range(bootstrap_N)])
-
-    print("#results = %d" % len(result))
-    x = pd.DataFrame(result, columns=['FTLD', 'AD', 'Common', 'overwrap_ratio', 'list_intersect'])
-    x.to_csv(os.path.join(Dir_output, "permutation_result_core_network_task%d.tsv" % TaskID), sep='\t')
+    x = pd.DataFrame(result, columns=['index', 'size_a', 'size_b', 'size_intersect', 'size_union', 'overwrap_ratio', 'intersection', 'union']).set_index('index')
+    x.to_csv(os.path.join(Dir_output, "permutation_result_for_core_network_task%d.tsv" % TaskID), sep='\t')
 
     ratios = x.loc[:,"overwrap_ratio"]
-    expected_ratio = len(common_proteins) / len(combined_list)
-    r = [o for o in ratios if o>expected_ratio]
-    print("probability of expected ratio (%f) is %f for %d permutations" % (expected_ratio, len(r)/len(ratios), len(ratios)))
+    r = [o for o in ratios if o >= expected_ratio]
+    print("%d samples were greater than or equal to the expected value" % len(r))
+    print("probability of expected ratio (%f) is %f for %d bootstrap samples" % (expected_ratio, len(r)/len(ratios), len(ratios)))
     
     pool.close()
     pool.terminate()
